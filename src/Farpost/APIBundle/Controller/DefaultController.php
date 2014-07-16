331b523ffc5e6250f64534bc97f356b31f4c2245
Mark Tertishniy <mtertishniy@gmail.com>
2014-07-16 12:39:54 +1100

    Добавляю выдачу аудиторий, преподавателей, предметов для группы

--- src/Farpost/APIBundle/Controller/DefaultController.php
+++ src/Farpost/APIBundle/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace Farpost\APIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Farpost\StoreBundle\Entity;
+use Doctrine\ORM\Query\Expr\Join;
 
 class DefaultController extends Controller
 {
@@ -46,13 +48,38 @@ class DefaultController extends Controller
 
    public function getForGroupAction($name)
    {
-      parse_str($_SERVER['QUERY_STRING']);
-      $response = new Response(json_encode([
-            'method' => 'getForGroup',
-            'name' => $name,
-            'group_id' => $group_id
-      ]));
-      $response->headers->set('Content-Type', 'application/json');
-      return $response;
+      $request = Request::createFromGlobals();
+      $response = $this->_CreateResponse();
+      $entities = [
+         'times'       => ['c' => 'Time',       'jf' => 'time',       'f' => ['end_time', 'start_time']],
+         'auditories'  => ['c' => 'Auditory',   'jf' => 'auditory',   'f' => ['alias', 'building_id', 'level_id', 'type_id']],
+         'professors'  => ['c' => 'User',       'jf' => 'professor',  'f' => ['first_name', 'middle_name', 'last_name']],
+         'disciplines' => ['c' => 'Discipline', 'jf' => 'discipline', 'f' => ['alias']]
+      ];
+      if (!$request->query->has('group') || empty($entities[$name])) return $response;
+      $table = $name != 'professors' ? $name : 'user';
+      $qb = $this->getDoctrine()
+                 ->getManager()
+                 ->getRepository('FarpostStoreBundle:' . $entities[$name]['c'])
+                 ->createQueryBuilder('t');
+      $join_field = 'id';
+      $sp_field =  $entities[$name]['jf'];
+      $join_preffix = 't';
+      if ($name == 'auditories' || $name == 'times') {
+         $qb->innerJoin('FarpostStoreBundle:Schedule', 's', Join::WITH, 't.id = s.' . $sp_field);
+         $join_preffix = 's';
+         $sp_field = 'id';
+         $join_field = 'schedule_part';
+      }
+      $qb->innerJoin(
+            'FarpostStoreBundle:SchedulePart',
+            'sp',
+            Join::WITH,
+            sprintf('sp.%s = %s.%s', $sp_field, $join_preffix, $join_field)
+      );
+      $res = $qb->select(array_map(function($a) { return "t.$a"; }, $entities[$name]['f']))
+                ->getQuery()
+                ->getArrayResult();
+      return $response->setStatusCode(200)->setContent(json_encode($res, JSON_UNESCAPED_UNICODE));
    }
 }
