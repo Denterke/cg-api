8249a0ae08f71cf9afb3feaf355433981dd5bbed
Kalita Ivan <kaduev13@gmail.com>
2014-09-26 20:23:14 +1100

    Admin authentification add

--- src/Farpost/WebBundle/Controller/DefaultController.php
+++ src/Farpost/WebBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FarpostWebBundle:Admin:login.html.twig');
+    	return $this->render("FarpostWebBundle:Default:index.html.twig");
     }
 }
