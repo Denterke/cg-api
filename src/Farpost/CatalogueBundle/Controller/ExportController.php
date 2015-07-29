06206adb57e7b5aa515f11e62303cb6c7df237f9
Иван Калита <kalita@vl.ru>
2015-07-29 18:23:38 +1000

    Спасаем связи в справочнике

--- src/Farpost/CatalogueBundle/Controller/ExportController.php
+++ src/Farpost/CatalogueBundle/Controller/ExportController.php
@@ -2,7 +2,11 @@
 
 namespace Farpost\CatalogueBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExportController extends Controller
 {
@@ -11,5 +15,22 @@ class ExportController extends Controller
         shell_exec("../app/console catalogue:export > /dev/null 2>/dev/null &");
         sleep(1);
         return $this->redirect($this->generateUrl('sonata_admin_dashboard'));
+    }
+
+    public function rescueAction()
+    {
+        $kernel = $this->get('kernel');
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'catalogue:rescue'
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+        return new Response($content);
     }
 }
