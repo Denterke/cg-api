7fe633dc646eee56957a4f733934e7d7d7fedf86
Иван Калита <kalita@vl.ru>
2015-08-17 13:08:35 +1000

    Вернул картиночки для категорий

--- src/Farpost/CatalogueBundle/Resources/public/js/categories.js
+++ src/Farpost/CatalogueBundle/Resources/public/js/categories.js
@@ -13,13 +13,20 @@ $(function() {
         $isOrganizationClicker = $isOrganization.next('ins'),
         objects = [
             $('[name*="[description]"]'),
-            $('[name*="[logoStandard][file]"]'),
-            $('[name*="[logoStandard][_delete]"]'),
-            $('[name*="[logoStandard][_delete]"]').closest('.form-group').parent().closest('.form-group'),
+            //$('[name*="[logoStandard][file]"]'),
+            //$('[name*="[logoStandard][_delete]"]'),
+            //$('[name*="[logoStandard][_delete]"]').closest('.form-group').parent().closest('.form-group'),
             $('[name*="[phone]"]'),
             $('[name*="[site]"]')
         ]
     ;
+
+    var el = $('.admin-preview');
+    if (el && el.attr('src')) {
+        var src =  el.attr('src');
+        src = src.substr(1, src.length - 1);
+        el.attr('src', src);
+    }
 
     objects = objects.map(function(object) {
         return object.closest('.form-group');
