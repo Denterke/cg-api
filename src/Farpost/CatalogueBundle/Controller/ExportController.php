8821d80875af5af10bdf4f012e8180bc52a1e6b3
Иван Калита <kalita@vl.ru>
2015-08-20 17:10:29 +1000

    Команды для спасения

--- src/Farpost/CatalogueBundle/Controller/ExportController.php
+++ src/Farpost/CatalogueBundle/Controller/ExportController.php
@@ -12,19 +12,19 @@ class ExportController extends Controller
 {
     public function exportAction()
     {
-        shell_exec("../app/console catalogue:export > /dev/null 2>/dev/null &");
+        shell_exec("../app/console catalogue:export > catalogue_export.log 2>catalogue_export_2.log &");
         sleep(1);
         return $this->redirect($this->generateUrl('sonata_admin_dashboard'));
     }
 
-    public function rescueAction()
+    private function performCommand($command)
     {
         $kernel = $this->get('kernel');
         $application = new Application($kernel);
         $application->setAutoExit(false);
 
         $input = new ArrayInput([
-            'command' => 'catalogue:rescue'
+            'command' => $command
         ]);
 
         $output = new BufferedOutput();
@@ -34,20 +34,23 @@ class ExportController extends Controller
         return new Response($content);
     }
 
+    public function rescueAction()
+    {
+        return $this->performCommand('catalogue:rescue');
+    }
+
+    public function fixBadEdgesAction()
+    {
+        return $this->performCommand('catalogue:fix');
+    }
+
+    public function resetProcessingCatalogueAction()
+    {
+        return $this->performCommand('catalogue:reset_processing');
+    }
+
     public function warnAction()
     {
-        $kernel = $this->get('kernel');
-        $application = new Application($kernel);
-        $application->setAutoExit(false);
-
-        $input = new ArrayInput([
-            'command' => 'catalogue:warn'
-        ]);
-
-        $output = new BufferedOutput();
-        $application->run($input, $output);
-
-        $content = $output->fetch();
-        return new Response($content);
+        return $this->performCommand('catalogue:warn');
     }
 }
